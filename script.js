f296395a536a38893a032c217377b70cdf1d54b6
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-01-26 17:38:43 -0600

    update loading screen logic, now awaits till music is loaded

--- script.js
+++ script.js
@@ -1,40 +1,15 @@
-import { AudioPlayer } from "./utils/audioplayer.js";
 import { loading_screen } from "./utils/loadingscreen.js";
 //TODO make the history, in deepseek there is a chat helping you
 
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
-    const loading_elements = [
-        { "game":document.getElementById("game") },
-        { "loading":document.getElementById("loading_screen") },
-        { "spinner_container":document.getElementById("spinner_container") },
-    ];
-
     const startbtn = document.getElementById("startbtn");
-    startbtn.addEventListener("click", async ()=>{
-        loading_screen(loading_elements);
+    
+    startbtn.addEventListener("click", ()=>{
+        const music_players = loading_screen();
         
 
-
-        // const menu_song = new AudioPlayer();    
-        // await menu_song.load("assets/sounds/cave_theme.ogg");
-    
-        // const ambience_song = new AudioPlayer();    
-        // await ambience_song.load("assets/sounds/dungeon_ambient.ogg");
-    
-        // const horror_song = new AudioPlayer();    
-        // await horror_song.load("assets/sounds/horror_ambient.ogg");
-    
-        // const goodend_song = new AudioPlayer();    
-        // await goodend_song.load("assets/sounds/A_Bitter_Hope_Half.wav");
-    
-        // const songs = {
-        //     "menu":menu_song,
-        //     "ambience":ambience_song,
-        //     "horror_1":horror_song,
-        //     "good_end":goodend_song
-        // }
 
     })
 
--- utils/loadingscreen.js
+++ utils/loadingscreen.js
@@ -1,3 +1,40 @@
-export function loading_screen(){
+import {load_music} from "./loadmusic.js"
+
+
+/**
+ * 
+ * @param {HTMLElement} game 
+ * @param {HTMLElement} startbtn 
+ * @param {HTMLElement} loading 
+ * @param {HTMLElement} spinner 
+ */
+export async function loading_screen() {
+    // when entering this func there has been already one interaction
+
+    const game = document.getElementById("game")
+    const startbtn = document.getElementById("startbtn")
+    const loading_screen = document.getElementById("loading_screen")
+    const spinner = document.getElementById("spinner_container")
+
+    startbtn.classList.add("hidden");
+    spinner.classList.remove("hidden");
+    const music_players = await load_music();
+
+
+    //!temporary solution????
+    //after three seconds start the app hoping everything is already loaded
+    // setInterval(()=>{
+
+    //     spinner.classList.add("hidden")
+    //     loading_screen.classList.add("fadeout")
+    //     game.classList.add("fadein")
+
+    // }, 1*1000);
     
+    spinner.classList.add("hidden")
+    loading_screen.classList.add("fadeout")
+    game.classList.add("fadein")
+
+
+    return music_players;
 }
