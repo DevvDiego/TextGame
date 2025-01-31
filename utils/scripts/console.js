8c8cfaf94da696679888091e758ebcad553cd126
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-01-31 16:09:34 -0600

    Add basic functionaity and fix padding to console

--- utils/scripts/console.js
+++ utils/scripts/console.js
@@ -3,7 +3,21 @@ export class Console{
         const system = document.getElementById("console_text");
         const input = document.getElementById("console_input");
 
-        
+        input.addEventListener("keypress", (ev)=>{
+            if(ev.key == "Enter"){
+                console.log("Nuevo texto entrante")
+                let text_in = input.value.toLowerCase();
+                
+
+                if(text_in == "hola"){
+                    
+                    system.innerText = "Hola, el juego apenas habla jaaaaaaaaaaaaa";
+                    console.log("texto de consola reemplazado");
+                }
+
+            }
+        });
+
     }
 
 
