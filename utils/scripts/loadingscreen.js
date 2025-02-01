1f9de775db6f6962632a6853a08112ce505809a4
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-01-31 20:10:27 -0600

    Add first story in json

--- utils/scripts/loadingscreen.js
+++ utils/scripts/loadingscreen.js
@@ -20,9 +20,9 @@ export async function loading_screen() {
     startbtn.classList.add("hidden");
     spinner.classList.remove("hidden");
 
-
+    // ? maybe add loading function? to load everything and have a single await
     const music_players = await load_music();
-    const console = new Console();
+    const console = new Console("assets/games/dungeon.json");
 
 
     spinner.classList.add("hidden")
