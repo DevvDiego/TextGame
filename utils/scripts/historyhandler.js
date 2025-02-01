45b34930d46fc07367cf740eb0a2bd97da6a217e
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-01-31 21:07:47 -0600

    Make a factory method to be able to make the code await for the promise.

--- utils/scripts/historyhandler.js
+++ utils/scripts/historyhandler.js
@@ -1,36 +1,37 @@
-export class HistoryHandler{
-    constructor(url){
-        this.history = this.load_history(url)
-        .then(()=>{
-            console.log("Historia cargada correctamente.");
-        })
-        .catch((error)=>{
-            console.error("Error al cargar la historia: " + error);
-        })
-
-
-
-        
-
+export class HistoryHandler {
+    constructor(history) {
+        this.history = history;
+        this.room = this.history["habitacion_1"]; // initial room
     }
 
+    // Factory method to handle asynchronously
+    static async create(url) {
+        const history = await HistoryHandler.load_history(url);
+        return new HistoryHandler(history); // Return a new instance with the loaded history
+    }
 
-
-    async load_history(url){
+    static async load_history(url) {
         try {
-            const response = await fetch(url); // Ruta al archivo JSON
+            const response = await fetch(url);
             if (!response.ok) {
                 throw new Error("No se pudo cargar el archivo JSON.");
-    
             }
-            
-            return await response.json(); 
-        
-        }catch(error){
+            return await response.json();
+        } catch (error) {
             throw error;
-    
         }
-    
+    }
+
+    what_room() {
+        return this.room;
+    }
+
+    what_desc() {
+        return this.room["descripcion"];
+    }
+
+    whato_options() {
+        return this.room["opciones"];
     }
 
 }
