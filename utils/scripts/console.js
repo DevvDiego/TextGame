3ce654150cd932cb475502fa6d38cff369713e47
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-01-31 21:43:28 -0600

    return arrow funct to be able to bind the "this" correctly to console instance

--- utils/scripts/console.js
+++ utils/scripts/console.js
@@ -2,25 +2,26 @@ import { HistoryHandler } from "./historyhandler.js";
 
 export class Console{
     constructor(history){
-        const system = document.getElementById("console_text");
-        const input = document.getElementById("console_input");
-        this.history_handler = new HistoryHandler(history);
+        this.system = document.getElementById("console_text");
+        this.input = document.getElementById("console_input");
+        this.history_handler = HistoryHandler.create(history);
        
-        input.addEventListener("keypress", this.process_input);
+        this.input.addEventListener("keypress", (ev)=>{
+            if(ev.key == "Enter"){
+                console.log("Nuevo texto entrante")
+                console.log(this.input)
+                let text_in = this.input.value.toLowerCase();
+                if(text_in == "habitacion"){
+                    this.system.innerText = "\n\n" + this.system.innerText + this.history_handler.what_room();
+                }
+    
+                if(text_in == "opciones"){
+                    this.system.innerText = "\n\n"+this.system.innerText + this.history_handler.what_options();
+                }
+            }
 
-    }
-
-    /**
-     * 
-     * @param {KeyboardEvent} ev 
-     */
-    process_input(ev){
-        if(ev.key == "Enter"){
-            console.log("Nuevo texto entrante")
-            let text_in = input.value.toLowerCase();
             
+        });
 
-        }
     }
-
 }
