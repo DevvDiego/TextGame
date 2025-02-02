7522268336206f543336c97007c7d9e92ee059d2
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-02-01 20:29:35 -0600

    Enable music correctly

--- utils/scripts/loadingscreen.js
+++ utils/scripts/loadingscreen.js
@@ -23,7 +23,7 @@ export async function loading_screen() {
     // ? maybe add loading function? to load everything and have a single await
 
     //TODO add specific loading bar locations depending on what await is done
-    // const music_players = await load_music();
+    const music_players = await load_music();
     const console = Console.create("assets/games/dungeon.json");
 
 
