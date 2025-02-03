ed9db47f33dd6bf476d3fee1855d3d7efe9cc90f
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-02-02 21:08:54 -0600

    upt methods for readability

--- utils/scripts/historyhandler.js
+++ utils/scripts/historyhandler.js
@@ -1,7 +1,28 @@
 export class HistoryHandler {
     constructor(history) {
-        this.history = history;
-        this.room = this.history["habitacion_1"]; // initial room
+        // this.history = history;
+        this.history = {
+            "habitacion_1": {
+                "nombre_habitacion": "Habitación Oscura",
+                "descripcion": "Estás en una habitación oscura. Hay una puerta al norte y una ventana al este.",
+                "opciones": [
+                    {
+                        "texto": "Abrir la puerta",
+                        "siguiente_habitacion": "habitacion_2"
+                    },
+                    {
+                        "texto": "Mirar por la ventana",
+                        "siguiente_habitacion": "habitacion_3"
+                    },
+                    {
+                        "texto": "Quedarse quieto",
+                        "siguiente_habitacion": "habitacion_1"
+                    }
+                ]
+            },
+        };
+
+        this.currentRoom = this.history.habitacion_1; // initial room
     }
 
     // Factory method to handle asynchronously
@@ -22,31 +43,21 @@ export class HistoryHandler {
         }
     }
 
-    what_room() {
-        // return this.room;
-        return "Habitacion 1";
+    roomName() {
+        return this.currentRoom.nombre_habitacion;
     }
 
-    what_desc() {
-        return this.room["descripcion"];
+    roomDesc() {
+        return this.currentRoom.descripcion;
     }
 
-    what_options() {
-        return this.#parse_options();
-    }
-
-    #parse_options() {
-        let opciones = this.room.opciones
+    roomOptions() {        
         let opcionesString = "Opciones:\n";
-    
-        // Recorrer las opciones y agregarlas al string
-        for (const key in opciones) {
-            if (opciones.hasOwnProperty(key)) {
-                const opcion = opciones[key];
-                opcionesString += `${key}: ${opcion.texto}\n`;
-            }
-        }
-    
+
+        this.currentRoom.opciones.forEach((opcion, index) => {
+            opcionesString += `${index + 1}: ${opcion.texto}\n` 
+        });
+
         return opcionesString;
     }
 
